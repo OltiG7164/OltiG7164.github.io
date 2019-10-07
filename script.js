701b26843e66368ecd4fa968356f09472ba873e6
OltiG7164 <56263478+OltiG7164@users.noreply.github.com>
2019-10-07 10:24:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Olti Gjoni"
+let userName = "OltiG7164";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
